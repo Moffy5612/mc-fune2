1a47adfcd7934427ce18eeb319009f6987d12947
Moffy5612 <a-eihave@outlook.jp>
2024-08-03 21:21:56 +0900

    assetsをコピーするよう変更

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,20 @@
-import { resolve } from 'path';
+import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets:[{
+        src: "assets/*",
+        dest: "assets"
+      }]
+    })
+  ],
   build: {
     // distフォルダに出力
     outDir: resolve(__dirname, '../dist'),
